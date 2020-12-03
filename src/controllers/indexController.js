bfac4abea10c13fe54b93536fb9ccc59bfd6861b
benjabarreiro <barreirobenjamin@gmail.com>
2020-12-03 14:55:39 -0300

    taller mercado pago - encuentro 3

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -12,9 +12,47 @@ module.exports = {
     detail: (req, res) => {
         return res.render("detail", { ...req.query });
     },
+    callback: (req, res) => {
+        console.log(req.query);
+
+        if(req.query.status.includes('success')){
+            res.render('success');
+        }
+
+        if(req.query.status.includes('pending')){
+            res.render('pending');
+        }
+
+        if(req.query.status.includes('failure')){
+            res.render('failure');
+        }
+
+        return res.render(404).end();
+    },
+
+    notifications: (req, res) => {
+        console.log(req.body);
+
+        res.status(200).end('ok');
+    },
+
     comprar: (req, res) => {
+        const host = 'https://localhost:3000/'
+        const url = host + 'callback?status=';
 
         let preference = {
+
+            //https://localhost:3000 --> se cambia por lo que esté en heroku
+
+            back_urls: {
+                success: url + 'success',
+                pending: url + 'pending',
+                failure: url + 'failure'
+            },
+
+            notification_url: host + 'notifications',
+
+            auto_return: 'approved',
 
             payer: {
                 name: 'Ryan',
@@ -43,12 +81,12 @@ module.exports = {
 
             items: [
                 {
-                    id: 1,
-                    picture_url: '',
+                    id: 1234,
+                    picture_url: 'https://mercadopago12345.herokuapp.com/images/products/jordan.jpg',
                     title: 'Nombre del producto',
-                    description: 'Descripción',
+                    description: 'Dispositivo móvil de Tienda e-commerce',
                     unit_price: 999,
-                    quantity: 3
+                    quantity: 1
                 }
             ],
 
