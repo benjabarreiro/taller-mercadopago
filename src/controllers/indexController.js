a5656876c5d42c489238dc1dfc2f22d88f1dd42f
benjabarreiro <barreirobenjamin@gmail.com>
2020-12-09 13:59:50 -0300

    asdf2

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -13,7 +13,7 @@ module.exports = {
         console.log(req.query);
 
         if(req.query.status.includes('success')){
-            return res.render('success', {
+            res.render('success', {
                 payment_type: req.query.payment_type,
                 external_reference: req.query.external_reference,
                 collection_id: req.query.collection_id
@@ -21,25 +21,24 @@ module.exports = {
         }
 
         if(req.query.status.includes('pending')){
-            return res.render('pending');
+            res.render('pending');
         }
 
         if(req.query.status.includes('failure')){
-            return res.render('failure');
+            res.render('failure');
         }
 
         return res.render(404).end();
     },
-    notifications: (req, res) => {
-        console.log("webhook", req.body);
-
-        res.status(200).end('ok');
-    },
-    
     detail: (req, res) => {
         return res.render("detail", { ...req.query });
     },
 
+    notifications: (req, res) => {
+        console.log(req.body);
+
+        res.status(200).end('ok');
+    },
 
     comprar: (req, res) => {
         const host = 'https://mercadopagodhclase.herokuapp.com/'
@@ -60,57 +59,50 @@ module.exports = {
             auto_return: 'approved',
 
             payer: {
-                name: "Lalo",
-                surname: "Landa",
-                email: "test_user_63274575@testuser.com",
+                name: 'Lalo',
+                surname: 'Landa',
+                email: 'test_user_63274575@testuser.com',
                 phone: {
                     area_code: '11',
-                    number: 22223333
+                    number: '22223333'
                 },
                 adress: {
                     zip_code: '1111',
                     street_name: 'False',
-                    street_number: 123
+                    street_number: '123'
                 }
             },
 
             payment_methods : {
                 excluded_payment_methods: [
-                    {
-                        id: "amex",
-                    },
+                    {id: 'amex'}
                 ],
                 excluded_payment_types: [
-                    {
-                        id: "atm",
-                    },
+                    {id: 'atm'}
                 ],
-                installments: 6,
+                installments: 6
             },
 
             items: [
                 {
                     id: 1234,
-                    picture_url: `https://mercadopago-ayr.herokuapp.com/images/products/${req.query.img}`,
-                    title: req.query.title,
-                    description: "Dispositivo móvil de Tienda e-commerce",
-                    unit_price: Number(req.query.price),
-                    quantity: Number(req.query.unit),
+                    picture_url: 'https://mercadopagodhclase.herokuapp.com/images/products/disruptor.jpg',
+                    title: 'Nombre del producto',
+                    description: 'Dispositivo móvil de Tienda e-commerce',
+                    unit_price: 9000,
+                    quantity: 1
                 }
             ],
 
             external_reference: 'barreirobenjamin@gmail.com'
         }
 
-        mercadopago.preferences
-            .create(preference)
-            .then(response => {
-                preference_init = response.body;
-            res.render("detail", { ...req.query, preference_init });
-        })
-        .catch(error => {
+        mercadopago.preferences.create(preference).then(response => {
+            global.init_point = response.body.init_point
+            res.render('confirm')
+        }).catch(error => {
             console.log(error)
-            res.send("error");
-        });
-    },
-};
+            res.send('error')
+        })
+    }
+}
